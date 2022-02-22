1fa804011fa89a8cf4fe27c783c88e5f91845018
Ngoc-Chau <playerzawesome@gmail.com>
2022-02-22 22:30:30 +0700

    Deleted the View anchor tag from task_list.html

--- base/urls.py
+++ base/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.Login.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("", views.TaskList.as_view(), name="tasks"),
-    path("task/<int:pk>/", views.TaskDetail.as_view(), name="task"),
     path("task-create/", views.TaskCreate.as_view(), name="task-create"),
     path(
         "task-update/<int:pk>", views.TaskUpdate.as_view(), name="task-update"
--- base/views.py
+++ base/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    FormView,
+)
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from django.urls import reverse_lazy
 
@@ -13,6 +19,23 @@ from .models import Task
 
 
 # Create your views here.
+class RegisterView(FormView):
+    form_class = UserCreationForm
+    template_name = "base/register.html"
+    success_url = reverse_lazy("tasks")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("tasks")
+        return super().get(request, *args, **kwargs)
+
+
 class Login(LoginView):
     template_name = "base/login.html"
     fields = "__all__"
@@ -30,12 +53,6 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context["tasks"] = context["tasks"].filter(user=self.request.user)
         return context
-
-
-class TaskDetail(LoginRequiredMixin, DetailView):
-    model = Task
-    context_object_name = "task"
-    template_name = "base/task.html"
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
